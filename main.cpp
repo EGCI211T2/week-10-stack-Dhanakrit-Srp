c4a4f9d11a28a340d8779d54443235ff4b0e0357
Dhanakrit-Srp <dhanakrit.srp@student.mahidol.edu>
2025-11-18 03:43:37 +0000

    sending work

--- main.cpp
+++ main.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 int main(int argc, char **argv){
     Stack s;
-     s.push(5);
-      s.push(1);
-      s.push(7);
-      s.pop();
-      s.push(6);
-      s.pop();
-
- /*
-  Exercise 1*/
     /*
+     s.push(5); //5
+      s.push(1); //1 5
+      s.push(7); //7 1 5
+      s.pop();// 1 5
+      s.push(6); //6 1 5 
+      s.pop();//1 5
+
+      */
+    
     int i;
-    for(i=1;i<argc;i++){
-      //  if(  x)  s.pop();
+    for(int i=1;i<argc;i++){
+        if(argv[i][0] == 'x')  s.pop();
         else s.push(atoi(argv[i]));
     }
-     */
+     
 
 
 
--- stack.h
+++ stack.h
@@ -17,11 +17,10 @@ void Stack::push(int x){
   NodePtr new_node=new NODE(x);
   if(new_node){
             new_node->set_next(top);
-            top= t->get_next();
+            top= new_node;
             size++;
    }
- 
-         cout<<"This is the top of stack"<<endl;
+   else cout<<"Not enough memory."<<endl;
     
 }
 
@@ -30,20 +29,34 @@ int Stack::pop(){
     if(t!=NULL){
         int value;
         value=t->get_value();
+
+        //move top to the next one
+        top=top->get_next();
+        
+        --size;
         
         delete t;
         return value;
 	
     }
+   cout<<"Empty stack"<<endl;
+    return 0;
 }
 
 Stack::Stack(){
     //initialize stack
+    top = NULL;
+    size = 0;
     
 }
 Stack::~Stack(){
     //delete all remaning stack (i.e. pop all) 
-    
+    cout<<"Clear the stack"<<endl;
+    int n = size;
+    while(n>0){
+    pop();
+    n--;
+    }
 }
 
 
